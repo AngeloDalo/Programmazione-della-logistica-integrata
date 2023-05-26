5e3879cc4654d7b4d8f2f73689a8fc4bb86dbe60
AD25 <angelo251158@gmail.com>
2023-05-26 20:13:02 +0200

    intro

--- resources/views/guest/intro.blade.php
+++ resources/views/guest/intro.blade.php
@@ -5,8 +5,31 @@
 @endsection
 
 @section('content')
-
-    <div class="footer mt-16">
+    <section>
+        <div class="bg-black text-white py-5">
+            <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+                <div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
+                    <h1 class="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">TechFest</h1>
+                    <h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Space : The Timeless Infinity
+                    </h2>
+                    <p class="text-sm md:text-base text-gray-50 mb-4">Explore your favourite events and
+                        register now to showcase your talent and win exciting prizes.</p>
+                    <a href="#"
+                        class="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
+                        Explore Now</a>
+                </div>
+                <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
+                    <div class="h-48 flex flex-wrap content-center">
+                        <div>
+                            <img class="inline-block mt-28 hidden xl:block"
+                                src="https://www.ideabit.com/album/foto_full/formato-immagini-webp_710.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="footer">
         <div class="bubbles">
             <div class="bubble"
                 style="--size:3.2412319835186203rem; --distance:6.881818062029767rem; --position:3.9089003580167514%; --time:2.229147241919517s; --delay:-2.916639444167297s;">
